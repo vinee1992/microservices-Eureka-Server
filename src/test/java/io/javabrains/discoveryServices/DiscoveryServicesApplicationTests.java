68af4f1e5c7c2808eee82ca336cc34c23d8ed78b
Vineet Chaurasia <vineetchaurasia2010@gmail.com>
2019-05-01 15:00:36 +0530

    Eureka Server

---
+++ src/test/java/io/javabrains/discoveryServices/DiscoveryServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package io.javabrains.discoveryServices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
